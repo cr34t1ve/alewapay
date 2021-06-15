1325a0132b8b6d1445fffbeeaebc851169e8db89
cr34t1ve <cr34t1ve@protonmail.com>
2021-06-15 02:26:13 +0000

    'intro screen 2' page initialised

---
+++ lib/screens/introscreen2/introscreen2.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class IntroScreen2 extends StatefulWidget {
+
+  @override
+  _IntroScreen2State createState() => _IntroScreen2State();
+}
+
+class _IntroScreen2State extends State<IntroScreen2> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+    );
+  }
+}
