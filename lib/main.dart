057ef5f5a4b3b8aa99840a8357ebf048f7a3bad3
cr34t1ve <cr34t1ve@protonmail.com>
2021-06-15 02:36:08 +0000

    'intro screen 2' page constructed

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:alewa_pay/screens/introscreen1/introscreen1.dart';
+import 'package:alewa_pay/screens/introscreen2/introscreen2.dart';
 import 'package:alewa_pay/screens/splash/splash.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: IntroScreen1(),
+    home: IntroScreen2(),
   ));
 }
 
--- lib/screens/introscreen2/introscreen2.dart
+++ lib/screens/introscreen2/introscreen2.dart
@@ -1,3 +1,4 @@
+import 'package:alewa_pay/components/button.dart';
 import 'package:flutter/material.dart';
 
 class IntroScreen2 extends StatefulWidget {
@@ -7,10 +8,72 @@ class IntroScreen2 extends StatefulWidget {
 }
 
 class _IntroScreen2State extends State<IntroScreen2> {
+  TextEditingController _otpController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 104.0,
+            ),
+            Row(
+              children: [
+                Text(
+                  'Please enter the code sent to\n0203392590',
+                  style: TextStyle(
+                      fontFamily: 'Manrope',
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w600,
+                      color: Color(0xFF243656)),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 15.0,
+            ),
+            TextField(
+              maxLength: 4,
+              controller: _otpController,
+              keyboardType: TextInputType.number,
+              style: TextStyle(
+                fontFamily: 'Manrope',
+                fontSize: 25.0,
+                fontWeight: FontWeight.w400,
+                color: Color(0xFF555E6C)
+              ),
+              decoration: InputDecoration(
+                  hintText: 'Confirmation Code',
+                  border: InputBorder.none,
+                  counterText: '',
+                  counterStyle:
+                                        TextStyle(height: double.minPositive),
+                  hintStyle: TextStyle(
+                      fontFamily: 'Manrope',
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w600,
+                      color: Color(0xFFCBD5E0))),
+            ),
+            SizedBox(
+              height: 40.0,
+            ),
+            Spacer(),
+            SafeArea(
+              child: DefaultButton(
+                text: 'Next',
+                press: () {},
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
