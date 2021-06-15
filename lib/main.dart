27a40e03b5cfd9cfea3edc66df8b5e28013172d8
cr34t1ve <cr34t1ve@protonmail.com>
2021-06-15 02:25:03 +0000

    'Intro Screen 1' directory change

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:alewa_pay/introscreen1/introscreen1.dart';
+import 'package:alewa_pay/screens/introscreen1/introscreen1.dart';
 import 'package:alewa_pay/screens/splash/splash.dart';
 import 'package:flutter/material.dart';
 
