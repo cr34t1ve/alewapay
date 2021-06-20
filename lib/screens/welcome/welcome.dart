cd0d74d2cd798cb6a394e243c665fc6ac35d13ba
cr34t1ve <cr34t1ve@protonmail.com>
2021-06-20 14:13:10 +0000

    'welcome' page constructed

--- lib/screens/welcome/welcome.dart
+++ lib/screens/welcome/welcome.dart
@@ -6,7 +6,27 @@ class Welcome extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xFF434190),
       body: Center(
-        child: Image.asset(''),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/giphy.gif',
+              height: 123.0,
+              width: 123.0,
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'Welcome to Alewa!!',
+              style: TextStyle(
+                  fontFamily: 'Manrope',
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w600,
+                  color: Color(0xFFF7FAFC)),
+            )
+          ],
+        ),
       ),
     );
   }
