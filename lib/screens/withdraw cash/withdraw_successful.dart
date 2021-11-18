51127f0fd9c06f170d8deb775207a3732c3f5742
Master Sofua Desmond <mrsofua@gmail.com>
2021-11-18 14:46:25 +0000

    withdraw successful page constructed

--- lib/screens/withdraw cash/withdraw_successful.dart
+++ lib/screens/withdraw cash/withdraw_successful.dart
@@ -1,3 +1,5 @@
+import 'package:alewa_pay/components/button.dart';
+import 'package:alewa_pay/enum/sizeConfig.dart';
 import 'package:flutter/material.dart';
 
 class WithdrawSuccessful extends StatefulWidget {
@@ -10,6 +12,52 @@ class WithdrawSuccessful extends StatefulWidget {
 class _WithdrawSuccessfulState extends State<WithdrawSuccessful> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/giphy.gif',
+                  height: getProportionateScreenHeight(123.0),
+                  width: getProportionateScreenWidth(123.0),
+                ),
+                SizedBox(
+                  height: getProportionateScreenHeight(10.0),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Your withdrawal of GHC 50.45\n was successful',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 22.0,
+                          fontWeight: FontWeight.w700,
+                          color: Color(0xFF243656)),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                SafeArea(child: DefaultButton(text: 'Done', press: () {})),
+                SizedBox(
+                  height: getProportionateScreenHeight(20.0),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
