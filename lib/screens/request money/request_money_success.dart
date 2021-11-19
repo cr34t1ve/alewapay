c3d8ca6b38ce4fc52150f280aa7f96d2907d6e51
Master Sofua Desmond <mrsofua@gmail.com>
2021-11-19 15:46:40 +0000

    request money success page constructed

--- lib/screens/request money/request_money_success.dart
+++ lib/screens/request money/request_money_success.dart
@@ -1,3 +1,5 @@
+import 'package:alewa_pay/components/button.dart';
+import 'package:alewa_pay/enum/sizeConfig.dart';
 import 'package:flutter/material.dart';
 
 class RequestMoneySuccess extends StatefulWidget {
@@ -8,8 +10,59 @@ class RequestMoneySuccess extends StatefulWidget {
 }
 
 class _RequestMoneySuccessState extends State<RequestMoneySuccess> {
-  @override
+   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/giphy.gif',
+                  height: getProportionateScreenHeight(123.0),
+                  width: getProportionateScreenWidth(123.0),
+                ),
+                SizedBox(
+                  height: getProportionateScreenHeight(10.0),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'You requested GHS 50.00\nfrom @osemuix',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 22.0,
+                          fontWeight: FontWeight.w700,
+                          color: Color(0xFF243656)),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                SafeArea(
+                    child: DefaultButton(
+                  text: 'Done',
+                  press: () {},
+                  isActive: true,
+                )),
+                SizedBox(
+                  height: getProportionateScreenHeight(20.0),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
