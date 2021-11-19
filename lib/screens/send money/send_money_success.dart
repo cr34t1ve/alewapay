5ac4c50c2b3c1ca6e8b6516c3a317a87e1b5428e
Master Sofua Desmond <mrsofua@gmail.com>
2021-11-19 15:32:40 +0000

    send money success page constructed

--- lib/screens/send money/send_money_success.dart
+++ lib/screens/send money/send_money_success.dart
@@ -1,3 +1,5 @@
+import 'package:alewa_pay/components/button.dart';
+import 'package:alewa_pay/enum/sizeConfig.dart';
 import 'package:flutter/material.dart';
 
 class SendMoneySuccess extends StatefulWidget {
@@ -10,6 +12,57 @@ class SendMoneySuccess extends StatefulWidget {
 class _SendMoneySuccessState extends State<SendMoneySuccess> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/giphy.gif',
+                  height: getProportionateScreenHeight(123.0),
+                  width: getProportionateScreenWidth(123.0),
+                ),
+                SizedBox(
+                  height: getProportionateScreenHeight(10.0),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'GHS 50.00 sent to @osemuix',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 22.0,
+                          fontWeight: FontWeight.w700,
+                          color: Color(0xFF243656)),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                SafeArea(
+                    child: DefaultButton(
+                  text: 'Done',
+                  press: () {},
+                  isActive: true,
+                )),
+                SizedBox(
+                  height: getProportionateScreenHeight(20.0),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
