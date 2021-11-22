c3286ebc371891629568b897c802e0e089627699
Master Sofua Desmond <mrsofua@gmail.com>
2021-11-22 18:44:32 +0000

    enter amount send scan page initialised

---
+++ lib/screens/scan_to_send/enter_amount_send_scan.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EnterAmountSendScan extends StatefulWidget {
+  const EnterAmountSendScan({Key? key}) : super(key: key);
+
+  @override
+  _EnterAmountSendScanState createState() => _EnterAmountSendScanState();
+}
+
+class _EnterAmountSendScanState extends State<EnterAmountSendScan> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
