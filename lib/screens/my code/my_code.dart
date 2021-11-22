30ca3ce1dcd4e08474c8e55246c06d3a602666c1
Master Sofua Desmond <mrsofua@gmail.com>
2021-11-22 19:18:27 +0000

    my code page constructed

--- lib/screens/my code/my_code.dart
+++ lib/screens/my code/my_code.dart
@@ -1,4 +1,6 @@
+import 'package:alewa_pay/enum/sizeConfig.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class MyCode extends StatefulWidget {
   const MyCode({Key? key}) : super(key: key);
@@ -8,8 +10,100 @@ class MyCode extends StatefulWidget {
 }
 
 class _MyCodeState extends State<MyCode> {
-  @override
+   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.close_outlined),
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: SvgPicture.asset('assets/images/Vectorsave.svg'))
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: getProportionateScreenHeight(120.0),
+          ),
+          Container(
+            height: getProportionateScreenHeight(239.0),
+            width: getProportionateScreenWidth(239.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                    color: Color(0xFF000000).withOpacity(0.10),
+                    blurRadius: 24.0,
+                    spreadRadius: 0,
+                    offset: Offset(0, 0))
+              ],
+            ),
+            child: Stack(
+              children: [
+                Center(
+                  child: Image.asset(
+                    'assets/images/image 7qr.png',
+                    scale: 1.0,
+                    height: getProportionateScreenHeight(225.24),
+                    width: getProportionateScreenWidth(225.24),
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    height: getProportionateScreenHeight(59.0),
+                    width: getProportionateScreenWidth(59.0),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                        color: Color(0xFFF7FAFC)),
+                    child: Center(
+                      child: Image.asset(
+                        'assets/images/image 8alewa.png',
+                        height: getProportionateScreenHeight(37.0),
+                        width: getProportionateScreenWidth(37.0),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: getProportionateScreenHeight(50.0),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '@osemuix',
+                style: Theme.of(context).textTheme.headline5,
+              )
+            ],
+          ),
+          SizedBox(
+            height: getProportionateScreenHeight(5.0),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Sebastian Livingstone',
+                style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w700,
+                    color: Color(0xFF243656)),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
